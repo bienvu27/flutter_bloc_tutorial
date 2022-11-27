2a45d8ee580a85c1db92aef7d46abe18a4356507
BienVu <bienvx1999@gmail.com>
2022-11-27 10:55:15 +0700

    Counter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_tutorial/count_app/bloc/counter_bloc.dart';
 
-import 'cubit/home_page.dart';
+import 'count_app/home.dart';
+
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(primarySwatch: Colors.blue),
     debugShowCheckedModeBanner: false,
-    home: const HomePage(),
+    home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: const HomePage()),
   ));
 }
 
